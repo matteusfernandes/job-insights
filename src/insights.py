02b2ac352a810f61c010dcfbfef824f9fc2b720b
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-27 14:51:30 -0300

    feat: create a function to filter job for by type

--- src/insights.py
+++ src/insights.py
@@ -13,21 +13,13 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filtered_jobs_types = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    for obj in jobs:
+        if obj["job_type"] == job_type:
+            filtered_jobs_types.append(obj)
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return filtered_jobs_types
 
 
 def get_unique_industries(path):
