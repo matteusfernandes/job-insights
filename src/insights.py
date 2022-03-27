efd8119beaf1c6a72c40b89cc8c6d7b875687531
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-27 15:45:40 -0300

    feat: Requirement 8 - create a function to filter salary range

--- src/insights.py
+++ src/insights.py
@@ -69,29 +69,20 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if "max_salary" not in job or "min_salary" not in job:
+        raise ValueError()
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    if (type(job["max_salary"]) is not int
+            or type(job["min_salary"]) is not int):
+        raise ValueError()
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError()
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if job["min_salary"] <= salary and salary <= job["max_salary"]:
+        return True
+
+    return False
 
 
 def filter_by_salary_range(jobs, salary):
