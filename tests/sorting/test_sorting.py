238bcce0756f5548d53483530616260a794de9db
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-27 16:07:09 -0300

    feat: Requirement 10 - create a test to function sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"max_salary": 5000, "min_salary": 1200, "date_posted": "2020-05-07"},
+        {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-06-12"},
+        {"max_salary": 15000, "min_salary": 500, "date_posted": "2020-06-19"},
+    ]
+
+    error = "wrong_criteria"
+
+    with pytest.raises(ValueError, match=f'invalid sorting criteria: {error}'):
+        sort_by(jobs, error)
